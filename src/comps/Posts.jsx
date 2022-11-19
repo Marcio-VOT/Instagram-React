084fbf9fce906c5ca9a02cdbee8d6d4377dd152c
MarcioJrPC <marciofsantos65@gmail.com>
2022-11-18 22:45:08 -0300

    finalizacaoDinamicos

--- src/comps/Posts.jsx
+++ src/comps/Posts.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 export default () => {
-  const esquerda = [
-    "heart-outline",
-    "chatbubble-outline",
-    "paper-plane-outline",
+  const postConteudo = [
+    {
+      usuarioIMG: "assets/meowed.svg",
+      usuario: "meowed",
+      conteudoIMG: "assets/gato-telefone.svg",
+      curtidoPor: "respondeai",
+      likesPost: 101.523,
+    },
+    {
+      usuarioIMG: "assets/barked.svg",
+      usuario: "barked",
+      conteudoIMG: "assets/dog.svg",
+      curtidoPor: "respondeai",
+      likesPost: 101.523,
+    },
   ];
-  const direita = ["bookmark-outline"];
   return (
     <div className="posts">
-      <Post
-        usuarioIMG="assets/meowed.svg"
-        usuario="meowed"
-        conteudoIMG="assets/gato-telefone.svg"
-        acoesEsquerda={esquerda}
-        acoesDireita={direita}
-      />
-      <Post
-        usuarioIMG="assets/barked.svg"
-        usuario="barked"
-        conteudoIMG="assets/dog.svg"
-        acoesEsquerda={esquerda}
-        acoesDireita={direita}
-      />
+      {postConteudo.map((f) => (
+        <Post
+          key={f.usuario}
+          usuarioIMG={f.usuarioIMG}
+          usuario={f.usuario}
+          conteudoIMG={f.conteudoIMG}
+          curtidoPor={f.curtidoPor}
+          likesPost={f.likesPost}
+        />
+      ))}
     </div>
   );
 };
@@ -65,26 +71,38 @@ function Conteudo(props) {
   );
 }
 function Fundo(props) {
+  const [likes, setLikes] = React.useState(false);
+  const [book, setBook] = React.useState(false);
   return (
     <div className="fundo">
       <div className="acoes">
         <div>
-          {props.fundo.acoesEsquerda.map((f) => (
-            <ion-icon key={f} name={f}></ion-icon>
-          ))}
+          <ion-icon
+            onClick={() => setLikes(!likes)}
+            name={likes ? "heart" : "heart-outline"}
+            style={likes ? { color: "red" } : { color: "" }}
+          ></ion-icon>
+          <ion-icon name="chatbubble-outline"></ion-icon>
+          <ion-icon name="paper-plane-outline"></ion-icon>
         </div>
         <div>
-          {props.fundo.acoesDireita.map((f) => (
-            <ion-icon key={f} name={f}></ion-icon>
-          ))}
+          <ion-icon
+            onClick={() => setBook(!book)}
+            name={book ? "bookmark" : "bookmark-outline"}
+          ></ion-icon>
         </div>
       </div>
 
       <div className="curtidas">
         <img src="assets/respondeai.svg" />
         <div className="texto">
-          Curtido por <strong>respondeai</strong> e
-          <strong>outras 101.523 pessoas</strong>
+          Curtido por <strong>{props.fundo.curtidoPor}</strong> e
+          <strong>
+            {" "}
+            outras{" "}
+            {likes ? props.fundo.likesPost + 0.001 : props.fundo.likesPost}{" "}
+            pessoas
+          </strong>
         </div>
       </div>
     </div>
